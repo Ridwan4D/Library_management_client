4705daf8459c84eb06ae51cb00654ba7132f62bf
Ridwanul Islam <ridwanul161@gmail.com>
2024-11-18 13:42:24 +0300

    update:footer responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,52 +1,184 @@
-import { FaTwitter, FaInstagram, FaFacebook } from 'react-icons/fa';
+import { FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="bg-gray-900">
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div className="grid row-gap-10 mb-8 lg:grid-cols-6 sm:grid-cols-2 md:grid-cols-4">
+        <div className="grid row-gap-10 mb-8 lg:grid-cols-6 sm:grid-cols-2 md:grid-cols-3">
           <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
             <div>
-              <p className="font-medium tracking-wide text-gray-300">Category</p>
+              <p className="font-medium tracking-wide text-gray-300">
+                Category
+              </p>
               <ul className="mt-2 space-y-2">
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">News</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">World</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Games</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">References</a></li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    News
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    World
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Games
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    References
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
               <p className="font-medium tracking-wide text-gray-300">Apples</p>
               <ul className="mt-2 space-y-2">
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Web</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">eCommerce</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Business</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Entertainment</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Portfolio</a></li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Web
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    eCommerce
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Business
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Entertainment
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Portfolio
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
               <p className="font-medium tracking-wide text-gray-300">Cherry</p>
               <ul className="mt-2 space-y-2">
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Media</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Brochure</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Nonprofit</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Educational</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Projects</a></li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Media
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Brochure
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Nonprofit
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Educational
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Projects
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
-              <p className="font-medium tracking-wide text-gray-300">Business</p>
+              <p className="font-medium tracking-wide text-gray-300">
+                Business
+              </p>
               <ul className="mt-2 space-y-2">
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Infopreneur</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Personal</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Wiki</a></li>
-                <li><a href="/" className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200">Forum</a></li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Infopreneur
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Personal
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Wiki
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                  >
+                    Forum
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
           <div className="md:max-w-md lg:col-span-2">
-            <span className="text-base font-medium tracking-wide text-gray-300">Subscribe for updates</span>
+            <span className="text-base font-medium tracking-wide text-gray-300">
+              Subscribe for updates
+            </span>
             <form className="flex flex-col mt-4 md:flex-row">
               <input
                 placeholder="Email"
@@ -62,20 +194,32 @@ const Footer = () => {
               </button>
             </form>
             <p className="mt-4 text-sm text-gray-500">
-              Bacon ipsum dolor amet short ribs pig sausage prosciuto chicken spare ribs salami.
+              Subscribe and become a member of Book Site, to get all update from
+              us.
             </p>
           </div>
         </div>
         <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
-          <p className="text-sm text-gray-500">© Copyright 2020 Lorem Inc. All rights reserved.</p>
+          <p className="text-sm text-gray-500">
+            © Copyright 2024 Book Site. All rights reserved.
+          </p>
           <div className="flex items-center mt-4 space-x-4 sm:mt-0">
-            <a href="/" className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400">
+            <a
+              href="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+            >
               <FaTwitter className="h-5 w-5" />
             </a>
-            <a href="/" className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400">
+            <a
+              href="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+            >
               <FaInstagram className="h-5 w-5" />
             </a>
-            <a href="/" className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400">
+            <a
+              href="/"
+              className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+            >
               <FaFacebook className="h-5 w-5" />
             </a>
           </div>
